65fd25793a422806893d56496914ed4ea159535f
MustafaCani <MustafaCani@users.noreply.github.com>
2026-09-04 02:45:42 +0000

    chore: update data to v98

--- js/suchedule.js
+++ js/suchedule.js
@@ -1,6 +1,6 @@
 const config = {
     term: '202601',
-    dataVersion: 97
+    dataVersion: 98
 };
 
 config.infoLink = `https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=${config.term}&crn_in=`;
