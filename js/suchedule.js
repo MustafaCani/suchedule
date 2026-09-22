5c0c9d6c60e768b8a115c496d99f4ecaff92f935
Mustafa Canıtez <35201805+MustafaCani@users.noreply.github.com>
2026-09-22 05:09:14 +0200

    chore: update data to v111 (#48)
    Co-authored-by: MustafaCani <MustafaCani@users.noreply.github.com>

--- js/suchedule.js
+++ js/suchedule.js
@@ -1,6 +1,6 @@
 const config = {
     term: '202602',
-    dataVersion: 110
+    dataVersion: 111
 };
 
 config.infoLink = `https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=${config.term}&crn_in=`;
